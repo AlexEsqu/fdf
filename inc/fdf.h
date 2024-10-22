e0cc052bfc04286e58238cc57ad20a7caa3ce9c6
Alex Kling <mkling@student.42.fr>
2024-10-23 01:12:28 +0200

    modified structures and defined pi values

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:52:14 by mkling            #+#    #+#             */
-/*   Updated: 2024/10/09 17:31:19 by mkling           ###   ########.fr       */
+/*   Updated: 2024/10/23 01:00:24 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,21 @@
 # include <stdlib.h>
 # include <limits.h>
 # include <stdbool.h>
+# include <math.h>
 # include <X11/keysym.h>
 # include <X11/X.h>
 # include "../lib/minilibx/mlx.h"
 # include "../lib/libft/inc/libft.h"
 
-# define WIN_WIDTH 500
-# define WIN_HEIGHT 500
+# define WIN_WIDTH 1200
+# define WIN_HEIGHT 1200
+# define ACCEPT_CHAR "abcdefABCDEF0123456789,x-+ "
+# define BLACK 0x000000
+# define WHITE 0xffffff
+# define PI				3.14159265358979323846
+# define PI_BY_FOUR		0.78539816339
+# define PI_BY_SIX		0.52359877559
+# define PI_BY_180		0.01745329251
 
 typedef unsigned char	t_byte;
 
@@ -35,11 +43,25 @@ enum	e_error
 	MALLOC_ERR = 1,
 };
 
-enum	e_dimension
+typedef struct s_point
 {
-	X = 0,
-	Y = 1,
-};
+	int		x;
+	int		y;
+	int		z;
+	int		dy;
+	int		dx;
+	int		color;
+}	t_point;
+
+typedef struct s_line
+{
+	t_point	origin;
+	t_point	end;
+	t_point	delta;
+	t_point	sign;
+	int		error;
+	int		error2;
+}	t_line;
 
 typedef struct s_image
 {
@@ -50,56 +72,56 @@ typedef struct s_image
 	int		endian;
 }	t_image;
 
+typedef struct s_grid
+{
+	t_point	*pts_array;
+	int		width;
+	int		height;
+	int		pts_count;
+}	t_grid;
+
 typedef struct s_display
 {
 	void	*link;
 	void	*window;
-	int		size_x;
-	int		size_y;
-	char	*title;
+	int		offset_x;
+	int		offset_y;
+	int		zoom;
+	int		unit;
+	double	tetha;
+	double	alpha;
+	double	gamma;
 	t_image	img;
+	t_grid	*grid;
 }	t_display;
 
-typedef struct s_point
-{
-	int		x;
-	int		y;
-}	t_pts;
-
-typedef struct s_line
-{
-	t_pts	origin;
-	t_pts	end;
-	t_pts	direction;
-	t_pts	sign;
-	int		error;
-	int		error2;
-}	t_line;
-
-typedef struct s_drawing
-{
-	int	x;
-	int	y;
-	int	width;
-	int	height;
-	int	color;
-}	t_drawing;
-
 /* DISPLAY */
-t_display	init_display(void);
-void		error_exit(char *error_message, t_display *display);
-void		success_exit(t_display *display);
+t_display	init_display(char *map_filepath);
+void		wipe(t_display *display);
+int			handle_input(int keysym, t_display *display);
+int			handle_mouse(int button, int x, int y, t_display *display);
 
 /* PAINTING */
-void	img_pixel_put(t_image *image, int x, int y, int color);
+void	put_pixel(t_image *image, int x, int y, int color);
+void	put_point(t_display *display, t_point point);
 int		encode_rgb(t_byte red, t_byte green, t_byte blue);
-int		paint_background(t_image *image, int color);
-int		paint_rectangle(t_image *image, t_drawing rectangle);
+void	paint_background(t_image *image, int color);
 int		render(t_display *display);
+t_point	apply_zoom_and_offset(t_point *point, t_display *display);
+t_point	isometrify(t_point point);
 
 /* PLOTTING */
-void	plot_line(t_pts *origin, t_pts *end, t_display *display);
+void	plot_line(t_point origin, t_point end, t_display *display);
 
+/* PARSING */
+void	parse_file_into_grid(char *map_filepath, t_display *display);
 
+/* ROTATING */
+void	rotate_z(t_display *display);
+void	rotate_y(t_display *display);
+void	rotate_x(t_display *display);
+
+/* ERRORS */
+void	exit_if(bool condition, char *error_message, t_display *display);
 
 #endif
